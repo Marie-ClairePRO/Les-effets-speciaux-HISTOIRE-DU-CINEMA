2912d43447eaf613716b22144e23eea96688dc95
Marie-ClairePRO <157795025+Marie-ClairePRO@users.noreply.github.com>
2024-01-26 14:58:21 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $name = $_POST["name"];
     header("Location: index.html?name=" . urlencode($name));
     exit();
